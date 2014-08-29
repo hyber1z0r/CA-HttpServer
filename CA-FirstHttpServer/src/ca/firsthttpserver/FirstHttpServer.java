fbfe89dd42b4f1988764ff3b0a91d1eaaebaffee
Filipovic <Filipovic@10.50.130.127>
2014-08-29 09:25:31 +0200

    StringBuilder er lavet i FirsthttpServer

--- CA-FirstHttpServer/src/ca/firsthttpserver/FirstHttpServer.java
+++ CA-FirstHttpServer/src/ca/firsthttpserver/FirstHttpServer.java
@@ -31,7 +31,19 @@ public class FirstHttpServer {
 
         @Override
         public void handle(HttpExchange he) throws IOException {
+            StringBuilder sb = new StringBuilder();
             String response = "Welcome to my very first almost home made Web Server :-)";
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy website</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<h2> Welcome to my very first home made web server :-*</h2>\n");
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+            response = sb.toString();
             he.sendResponseHeaders(200, response.length());
             try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
                 pw.print(response); //What happens if we use a println instead of print --> Explain
