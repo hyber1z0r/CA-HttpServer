05d07d09e2db6e8cd90e9b9e171ba859ebfe7654
jakobgaardandersen <jakobgaardandersen@10.50.130.130>
2014-08-29 10:03:07 +0200

    Added page that shows Request Headers

--- CA-FirstHttpServer/src/ca/firsthttpserver/FirstHttpServer.java
+++ CA-FirstHttpServer/src/ca/firsthttpserver/FirstHttpServer.java
@@ -7,6 +7,8 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Lars Mortensen
@@ -23,6 +25,7 @@ public class FirstHttpServer {
         }
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/welcome", new RequestHandler());
+        server.createContext("/headers", new RequestHandler2());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
@@ -45,6 +48,39 @@ public class FirstHttpServer {
             sb.append("</body>\n");
             sb.append("</html>\n");
             response = sb.toString();
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+            he.sendResponseHeaders(200, response.length());
+            try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
+                pw.print(response); //What happens if we use a println instead of print --> Explain
+            }
+        }
+    }
+
+    static class RequestHandler2 implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            StringBuilder sb = new StringBuilder();
+            sb.append("<!DOCTYPE html>\n");
+            sb.append("<html>\n");
+            sb.append("<head>\n");
+            sb.append("<title>My fancy website</title>\n");
+            sb.append("<meta charset='UTF-8'>\n");
+            sb.append("</head>\n");
+            sb.append("<body>\n");
+            sb.append("<table border=\"1\">\n");
+            sb.append("<tr><th>Header</th><th>Value</th></tr>\n");
+            
+            Headers heads = he.getRequestHeaders();
+            for (Map.Entry<String, List<String>> entry : heads.entrySet()) {
+                sb.append("<tr><td>"+ entry.getKey() + "</td><td>"+ entry.getValue()+ "</td></tr>\n");
+            }
+            sb.append("</table>\n");
+            sb.append("</body>\n");
+            sb.append("</html>\n");
+
+            String response = sb.toString();
             Headers h = he.getResponseHeaders();
             h.add("Content-Type", "text/html");
             he.sendResponseHeaders(200, response.length());
