7138d96afff6b19896a7c75a7de3a59e7eab3704
Filipovic <Filipovic@10.50.130.127>
2014-08-29 10:30:04 +0200

    Added Worlds simplets Web-Server

--- CA-FirstHttpServer/src/ca/firsthttpserver/FirstHttpServer.java
+++ CA-FirstHttpServer/src/ca/firsthttpserver/FirstHttpServer.java
@@ -4,7 +4,11 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.util.List;
@@ -17,15 +21,18 @@ public class FirstHttpServer {
 
     static int port = 8080;
     static String ip = "127.0.0.1";
+    static String contentFolder = "public/";
 
     public static void main(String[] args) throws Exception {
-        if (args.length == 2) {
-            port = Integer.parseInt(args[0]);
+        if (args.length == 3) {
+            port = Integer.parseInt(args[1]);
             ip = args[0];
+            contentFolder = args[2];
         }
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/welcome", new RequestHandler());
         server.createContext("/headers", new RequestHandler2());
+        server.createContext("/pages/", new RequestHandler3());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
@@ -71,10 +78,10 @@ public class FirstHttpServer {
             sb.append("<body>\n");
             sb.append("<table border=\"1\">\n");
             sb.append("<tr><th>Header</th><th>Value</th></tr>\n");
-            
+
             Headers heads = he.getRequestHeaders();
             for (Map.Entry<String, List<String>> entry : heads.entrySet()) {
-                sb.append("<tr><td>"+ entry.getKey() + "</td><td>"+ entry.getValue()+ "</td></tr>\n");
+                sb.append("<tr><td>" + entry.getKey() + "</td><td>" + entry.getValue() + "</td></tr>\n");
             }
             sb.append("</table>\n");
             sb.append("</body>\n");
@@ -87,6 +94,28 @@ public class FirstHttpServer {
             try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
                 pw.print(response); //What happens if we use a println instead of print --> Explain
             }
+
+        }
+    }
+
+    static class RequestHandler3 implements HttpHandler {
+
+        @Override
+
+        public void handle(HttpExchange he) throws IOException {
+            File file = new File(contentFolder + "index.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend, 0, bytesToSend.length);
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            }
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend, 0, bytesToSend.length);
+            }
+
         }
     }
 }
